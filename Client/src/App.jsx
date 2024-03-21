e0adc43b5843a4cc270f16432ca43d6a63877271
SubhamMohanta <subhammohanta99@gmail.com>
2024-03-21 16:10:42 +0530

    Added components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Landing from './components/Landing'
+import Contact from './components/Contact'
+import Pricing from './components/Pricing'
 
 function App() {
 
   return (
     <>
-      <Landing/>
+        <Routes>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/Contact' element={<Contact/>}/>
+          <Route path='/Pricing' element={<Pricing/>}/>
+        </Routes>
     </>
   )
 }
