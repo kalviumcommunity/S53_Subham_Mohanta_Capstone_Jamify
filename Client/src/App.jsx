96e884360477de90077ebdd527f5ee9925918f10
SubhamMohanta <subhammohanta99@gmail.com>
2024-03-26 18:00:30 +0530

    dashboard

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,17 +5,20 @@ import Landing from './components/Landing'
 import Contact from './components/Contact'
 import Pricing from './components/Pricing'
 import About from './components/About'
+import Sidebar from './components/Sidebar'
+import Dashboard from './components/Dashboard'
 
 function App() {
 
   return (
     <>
-        <Routes>
+        {/* <Routes>
           <Route path='/' element={<Landing/>}/>
           <Route path='/Contact' element={<Contact/>}/>
           <Route path='/Pricing' element={<Pricing/>}/>
           <Route path='/About' element={<About/>}/>
-        </Routes>
+        </Routes> */}
+        <Dashboard/>
     </>
   )
 }
